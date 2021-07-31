698d4163264f6ffe896c95125aed59cefa7a31fc
Catherine Hodgetts <cat@Cats-MacBook-Pro.local>
2021-07-31 17:25:12 +0100

    Fix 3

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import Login from "./Pages/login";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from "./Pages/home";
+import Stock from "./Pages/Stock";
 
 ReactDOM.render(
     <Router>
         <Switch>
             <Route exact path="/" component={Login}/>,
             <Route exact path="/Pages/home" component={Home}/>,
+            <Route exact path="/Pages/Stock" component={Stock}/>,
         </Switch>
     </Router>,
 
